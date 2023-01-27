d78cf3b04cd450540815499fe929417bf33dc8df
ilyakoren <IlyaKoren@coherentsolutions.com>
2023-01-27 03:35:25 +0300

    added telegram message_handler

--- bot/views.py
+++ bot/views.py
@@ -1,10 +1,15 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+import os
+import telebot
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
 
 
 def homepage(request):
     return HttpResponse("<h1>Hello, Dinka Koren</h1>")
 
 
-def webhook(request):
-    return HttpResponse("test")
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
