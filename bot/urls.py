6eb62d6dd00b0fc42989883f0bd69e0018cb4b70
ilyakoren <IlyaKoren@coherentsolutions.com>
2023-01-27 15:04:08 +0300

    delete webhook

--- bot/urls.py
+++ bot/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.homepage, name='index'),
-    path("webhook", views.webhook, name="webhook"),
+    path("", views.homepage, name='index')
 ]
